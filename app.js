1dafaff2d85f7f3d8360baeb9343c93fe89a1f7a
Jazzwall Sharad <jazzwall@Jazzwalls-iMac.attlocal.net>
2019-03-29 00:04:35 -0700

    added more ninja stuff

--- app.js
+++ app.js
@@ -6,19 +6,22 @@ class Hello extends React.Component
         super();
         this.state =
         {
-            message: "my friend!!"
+            toggleSwitch: true
+            //message: "my friend!!"
         };
+        this.toggleListType = this.toggleListType.bind(this);
+        this.callAPI = this.callAPI.bind(this);
 
-        this.updateMessage = this.updateMessage.bind(this);
+        //this.updateMessage = this.updateMessage.bind(this);
     }
 
-    updateMessage()
-    {
-        this.setState
-        (
-            {message: this.state.storySoFar}
-        );
-    }
+    // updateMessage()
+    // {
+    //     this.setState
+    //     (
+    //         {message: this.state.storySoFar}
+    //     );
+    // }
 
     componentDidMount()
     {
@@ -27,14 +30,43 @@ class Hello extends React.Component
         .then(i=>this.setState({storySoFar: i}));
     }
 
+    callAPI()
+    {
+        fetch("https://storytimeapi.azurewebsites.net/api/values")
+        .then(r=>r.text())
+        .then(i=>this.setState({storySoFar: i}));  
+    }
+
+    toggleListType()
+    {
+        this.setState({toggleSwitch: !this.state.toggleSwitch});
+    }
+
     render()
     {
-        return(
-            <div>
-            <h1>Hello {this.state.message}!</h1>
-            <button onClick={this.updateMessage}>Click Me!</button>
-            </div>
-        )
+        var ts = this.state.toggleSwitch;
+
+        var cc =
+        [
+            <h1>Story So Far!</h1>,
+            <button onClick={this.toggleListType}>Click Me!</button>
+        ]
+
+        var a = 
+        <div> {cc}
+            <ul>
+                <li>{this.state.storySoFar}</li>
+            </ul>
+        </div>
+
+        var b = 
+        <div> {cc}
+            <ol>
+                <li>{this.state.storySoFar}</li>
+            </ol>
+        </div>
+
+        return(ts?a:b);
     }
 }
 
